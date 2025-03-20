05fde8268c175fff2c579e19a2bb3e09a686e600
Kripesh Nihure <nkripesh0@gmail.com>
2025-03-20 21:44:10 +0545

    modified CI/CD using ansible

--- Backend/demo/car_parts-2.sql
+++ Backend/demo/car_parts-2.sql
@@ -58,23 +58,9 @@ CREATE TABLE `suppliers` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `suppliers`
---
 
 LOCK TABLES `suppliers` WRITE;
 /*!40000 ALTER TABLE `suppliers` DISABLE KEYS */;
 INSERT INTO `suppliers` VALUES (1,'mercedes','stuttgart','123@jamal','germany'),(2,'ferrari','monza','123@jamal','italy'),(3,'mahle','stuttgart','123@jamal','germany'),(4,'brembo','bergamo','123@jamal','italy'),(5,'renault','marseille','123@jamal','france'),(6,'alcon','tamworth','123@jamal','england'),(7,'shell','london','123@jamal','england'),(8,'pirelli','milan','123@jamal','italy'),(9,'petronas','kuala lampur','123@jamal','malaysia'),(10,'michelin','clermont','123@jamal','france');
 /*!40000 ALTER TABLE `suppliers` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-09-22 15:43:48
--- Backend/demo/car_parts.sql
+++ Backend/demo/car_parts.sql
@@ -1,79 +1,50 @@
--- MySQL dump 10.13  Distrib 8.0.35, for Win64 (x86_64)
---
--- Host: localhost    Database: car_parts
--- ------------------------------------------------------
--- Server version	8.0.35
+-- Create the database
+SET time_zone = '+00:00';
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE IF NOT EXISTS car_parts;
+USE car_parts;
 
---
--- Table structure for table `items`
---
-create database car_parts;
-use car_parts;
-
-DROP TABLE IF EXISTS `items`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `items` (
+-- Create the items table
+DROP TABLE IF EXISTS `Items`;
+CREATE TABLE `Items` (
   `itemID` int NOT NULL,
   `name` varchar(20) DEFAULT NULL,
   `price` int DEFAULT NULL,
   `category` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`itemID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `items`
---
-
-LOCK TABLES `items` WRITE;
-/*!40000 ALTER TABLE `items` DISABLE KEYS */;
-INSERT INTO `items` VALUES (129,'petronas',1200,'lubricant'),(132,'shell',1300,'lubricant'),(157,'renault',9800,'engine'),(234,'mahle',4400,'pistons'),(256,'brembo',3200,'brakes'),(257,'alcon',3350,'brakes'),(354,'pirelli',3400,'tire'),(355,'michelin',3600,'tire'),(467,'mercedes',9900,'engine'),(477,'ferrari',9700,'engine');
-/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+-- Insert data into the items table
+LOCK TABLES `Items` WRITE;
+INSERT INTO `Items` VALUES 
+(129,'petronas',1200,'lubricant'),
+(132,'shell',1300,'lubricant'),
+(157,'renault',9800,'engine'),
+(234,'mahle',4400,'pistons'),
+(256,'brembo',3200,'brakes'),
+(257,'alcon',3350,'brakes'),
+(354,'pirelli',3400,'tire'),
+(355,'michelin',3600,'tire'),
+(467,'mercedes',9900,'engine'),
+(477,'ferrari',9700,'engine');
 UNLOCK TABLES;
 
---
--- Table structure for table `stores`
---
-
-DROP TABLE IF EXISTS `stores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `stores` (
+-- Create the stores table
+DROP TABLE IF EXISTS `Stores`;
+CREATE TABLE `Stores` (
   `location` varchar(20) DEFAULT NULL,
   `store_manager` varchar(20) DEFAULT NULL,
   `size` varchar(10) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+);
 
---
--- Dumping data for table `stores`
---
-
-LOCK TABLES `stores` WRITE;
-/*!40000 ALTER TABLE `stores` DISABLE KEYS */;
-INSERT INTO `stores` VALUES ('lazimpat','senna','large'),('pulchowk','schumacher','medium'),('ason','vettel','small'),('tahachal','alonso','small'),('durbarmarg','hamilton','large'),('patan','raikonnen','large'),('durbarmarg','fittipaldi','medium');
-/*!40000 ALTER TABLE `stores` ENABLE KEYS */;
+-- Insert data into the stores table
+LOCK TABLES `Stores` WRITE;
+INSERT INTO `Stores` VALUES 
+('lazimpat','senna','large'),
+('pulchowk','schumacher','medium'),
+('ason','vettel','small'),
+('tahachal','alonso','small'),
+('durbarmarg','hamilton','large'),
+('patan','raikonnen','large'),
+('durbarmarg','fittipaldi','medium');
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-09-21 22:12:31
